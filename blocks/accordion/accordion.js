3400b01326ad1f62873160280c28893ed622c545
Stephan Ringel <ringel@adobe.com>
2022-12-05 14:35:06 +0100

    add some icons

--- blocks/accordion/accordion.js
+++ blocks/accordion/accordion.js
@@ -1,16 +1,16 @@
 const template = props => `
-  <mj-section css-class="section">
-    <mj-column css-class="column">
-      <mj-accordion css-class="accordion">
-        ${props.items.map(item => `
-        <mj-accordion-element css-class="accordion-element">
-          <mj-accordion-title css-class="accordion-title">${item.title}</mj-accordion-title>
-          <mj-accordion-text css-class="accordion-text">${item.text}</mj-accordion-text>
-        </mj-accordion-element>
-        `).join('')}
-      </mj-accordion>
-    </mj-column>
-  </mj-section>
+  <mj-accordion css-class="accordion">
+    ${props.items.map(item => `
+    <mj-accordion-element css-class="accordion-element"
+      icon-wrapped-url="/icons/circle_chevron_down_icon.svg"
+      icon-unwrapped-url="/icons/circle_chevron_up_icon.svg"
+      icon-height="24px"
+      icon-width="24px">
+      <mj-accordion-title css-class="accordion-title">${item.title}</mj-accordion-title>
+      <mj-accordion-text css-class="accordion-text">${item.text}</mj-accordion-text>
+    </mj-accordion-element>
+    `).join('')}
+  </mj-accordion>
 `
 
 export default function decorate(block) {
