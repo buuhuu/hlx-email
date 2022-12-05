b5529b832e94a63cfe58bda8239b428fc2703284
Stephan Ringel <ringel@adobe.com>
2022-12-05 15:12:11 +0100

    wrap with section and column

--- blocks/carousel/carousel.js
+++ blocks/carousel/carousel.js
@@ -1,9 +1,13 @@
 const template = (props) => `
-  <mj-carousel css-class="plop" left-icon="/icons/circle_chevron_left_icon.svg" right-icon="icons/circle_chevron_right_icon.svg" icon-width="24px">
-    ${props.items.map((item) => `
-    <mj-carousel-image css-class="carousel-image" src="${item.src}" />
-    `).join('')}
-  </mj-carousel>
+  <mj-section>
+    <mj-column>
+      <mj-carousel css-class="plop" left-icon="/icons/circle_chevron_left_icon.svg" right-icon="icons/circle_chevron_right_icon.svg" icon-width="24px">
+        ${props.items.map((item) => `
+        <mj-carousel-image css-class="carousel-image" src="${item.src}" />
+        `).join('')}
+      </mj-carousel>
+    </mj-column>
+  </mj-section>
 `;
 
 export default function decorate(block) {
