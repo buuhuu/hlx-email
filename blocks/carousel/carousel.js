1adbfac7315337ba2784c615cce23558dfb57976
Stephan Ringel <ringel@adobe.com>
2022-12-06 11:18:46 +0100

    use jpg for icons and hlx webp image format

--- blocks/carousel/carousel.js
+++ blocks/carousel/carousel.js
@@ -1,7 +1,7 @@
 const template = (props) => `
   <mj-section>
     <mj-column>
-      <mj-carousel css-class="plop" left-icon="/icons/circle_chevron_left_icon.svg" right-icon="icons/circle_chevron_right_icon.svg" icon-width="24px">
+      <mj-carousel css-class="plop" left-icon="/icons/circle_chevron_left_icon.jpg" right-icon="icons/circle_chevron_right_icon.jpg" icon-width="24px">
         ${props.items.map((item) => `
         <mj-carousel-image css-class="carousel-image" src="${item.src}" />
         `).join('')}
@@ -11,7 +11,7 @@ const template = (props) => `
 `;
 
 export default function decorate(block) {
-  const items = Array.from(block.querySelectorAll(':scope picture source:last-of-type')).map((item) => ({
+  const items = Array.from(block.querySelectorAll(':scope picture source[type="image/webp"]:not([media])')).map((item) => ({
     src: item.srcset,
   }));
 
--- tools/sidekick/plugins.js
+++ tools/sidekick/plugins.js
@@ -1,9 +1,9 @@
 class EmailSidekickBanner {
-    constructor(id) {
-      this.banner = document.createElement('div');
-      this.banner.className = 'email-sidekick-banner';
-      this.banner.id = id;
-      this.banner.appendChild(document.createElement('style')).textContent = `
+  constructor(id) {
+    this.banner = document.createElement('div');
+    this.banner.className = 'email-sidekick-banner';
+    this.banner.id = id;
+    this.banner.appendChild(document.createElement('style')).textContent = `
       .email-sidekick-banner {
         z-index: 9999998;
         position: fixed;
@@ -40,37 +40,37 @@ class EmailSidekickBanner {
         user-selection: none;
         cursor: pointer;
       }`;
-      this.bannerContent = this.banner.appendChild(document.createElement('p'));
-      this.bannerContent.className = 'content';
-      document.body.prepend(this.banner);
-    }
-  
-    querySelector(selector) {
-      return this.bannerContent.querySelector(selector);
-    }
-  
-    write(content, timeout) {
-      this.bannerContent.innerHTML = content;
-      if (timeout) {
-        this.hide(timeout);
-      }
-    }
-  
-    hide(timeout = 0) {
-      setTimeout(() => {
-        this.banner.remove();
-      }, timeout * 1000);
+    this.bannerContent = this.banner.appendChild(document.createElement('p'));
+    this.bannerContent.className = 'content';
+    document.body.prepend(this.banner);
+  }
+
+  querySelector(selector) {
+    return this.bannerContent.querySelector(selector);
+  }
+
+  write(content, timeout) {
+    this.bannerContent.innerHTML = content;
+    if (timeout) {
+      this.hide(timeout);
     }
   }
 
+  hide(timeout = 0) {
+    setTimeout(() => {
+      this.banner.remove();
+    }, timeout * 1000);
+  }
+}
+
 const copyHtml = ({ detail }) => {
-    const iframe = document.querySelector('iframe');
-    if (iframe) {
-        navigator.clipboard.writeText(iframe.contentWindow.document.documentElement.innerHTML).then(() => {
-            const banner = new EmailSidekickBanner('get-html-data');
-            banner.write('HTML copied to clipboard', 5);
-        });
-    }
+  const iframe = document.querySelector('iframe');
+  if (iframe) {
+    navigator.clipboard.writeText(iframe.contentWindow.document.documentElement.innerHTML).then(() => {
+      const banner = new EmailSidekickBanner('get-html-data');
+      banner.write('HTML copied to clipboard', 5);
+    });
+  }
 };
 
 const sk = document.querySelector('helix-sidekick');
